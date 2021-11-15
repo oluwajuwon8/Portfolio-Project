b701f6a78353c9e031af12c67454ad4a7f676786
oluwajuwon8 <azeezatlola@gmail.com>
2021-11-15 17:35:20 +0100

    Add javascript file

---
+++ script.js
@@ -0,0 +1,19 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "320px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+function hidenavblock() {
+    const x = document.getElementById('mySidebar');
+    if (x.style.display === 'none') {
+    x.style.display = 'block';
+    } else {
+    x.style.display = 'none';
+    }
+    window.location.reload();
+}
